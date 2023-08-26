f7f5d9e2f1a489f303734165c8bdbd4a0642f5d4
Parker Bowling <pbowlin1@vols.utk.edu>
2023-08-26 08:49:02 -0400

    added links to database items

--- mywebsite/recipes/urls.py
+++ mywebsite/recipes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import RecipeListView, RecipeAddView, RecipeDetailView, RecipeSearchView
+from .views import RecipeListView, RecipeAddView, RecipeDetailView
 
 #declares app name for namespace
 app_name = 'recipes'
@@ -10,6 +10,6 @@ urlpatterns = [
     path('recipe_home/',RecipeListView.as_view(),name='recipe_home'),
     path('add_recipe/',RecipeAddView.as_view(),name='add_recipe'),
     path('<int:id>/',RecipeDetailView.as_view(),name='recipe_detail'),
-    path('search_recipe/',RecipeSearchView.as_view(),name='search_recipe')
+    # path('search_recipe/',RecipeSearchView.as_view(),name='search_recipe')
     
 ]
--- mywebsite/recipes/views.py
+++ mywebsite/recipes/views.py
@@ -41,19 +41,3 @@ class RecipeAddView(CreateView):
     def form_valid(self, form):
         print(form.cleaned_data)
         return super().form_valid(form)
-    
-class RecipeSearchView(TemplateView):
-    template_name = "recipe_search.html"
-    
-    def get(self, request, *args, **kwargs):
-        q = request.GET.get('q')
-        
-        if q:
-            vector = SearchVector('title')
-            query = SearchQuery(q)
-            self.results = recipesModel.objects.annotate(rank=SearchRank(vector,query)).filter(rank__gte=0.001).order_by('-rank')
-        
-        return super().get(request, *args, **kwargs)
-    
-    def get_context_data(self, **kwargs):
-        return super().get_context_data(results=self.results, **kwargs)
