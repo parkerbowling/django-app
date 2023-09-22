8bbb9951eae0d9753337e1c8901a53b54dbd0ddc
parkerbowling <parkerdbowling@gmail.com>
2023-09-22 14:44:29 -0400

    sankey diagram now works, still need to think about if I want to filter by timeframe

--- mywebsite/expenses/views.py
+++ mywebsite/expenses/views.py
@@ -4,6 +4,7 @@ from .models import expenseReport
 from .forms import expenseReportForm
 from django.contrib import messages
 from django.http import JsonResponse
+from datetime import datetime
 
 #temporary home page for now
 def home(request):
@@ -47,23 +48,52 @@ def chart_data(request):
     
     #for every category (remember not all categories may have appeared yet) sum the values and add to data
     #will need to be able to filter on certain dates, like months and years
+    dateNow = datetime.now()
+    currentYear = dateNow.year
+    currentMonth = dateNow.month
+    print(currentYear)
+    print(currentMonth)
+    
     for i in newSet:
         
         #gets all values for category and sums
-        categorySum = expenseReport.objects.values("value").filter(expenseChoices=str(i)).aggregate(Sum('value'))['value__sum']
+        if True:
+            categorySum = expenseReport.objects.values("value"
+                    ).filter(
+                        date__year=currentYear, date__month=currentMonth
+                    ).filter(
+                        expenseChoices=str(i)).aggregate(
+                    Sum('value'))['value__sum']
+                        
+        # will need an else statement if user wants to see lifetime expenses (all)
+        # else:
+        #
+        #     categorySum = expenseReport.objects.values("value"
+        #             ).filter(
+        #                 expenseChoices=str(i)).aggregate(
+        #             Sum('value'))['value__sum']
+            
+        #if there is no data, no need to add to chart     
+        if categorySum == None:
+            continue
 
         data = {
             'name':str(i),
             'y': float(categorySum)
         }
         allCategoryData['data'].append(data)
-    
-    #need to include updating for month and year selection
-    
+        
+    #depending on selection, change this
+    chartTitle = ""
+    if True:
+        chartTitle = f"Total Expenses This Month {currentMonth}/{currentYear}"
+    # else:
+    #     chartTitle = "Total Expenses All"
+        
     #json chart configuration
     chart = {
-        'chart': {'type': 'pie', 'renderTo':'container0'},
-        'title': {'text': 'Total Expenses'},
+        'chart': {'type': 'pie', 'renderTo':'expenses-pie-container'},
+        'title': {'text': chartTitle},
         'tooltip': {
             'format': 
                 '{series.name}: <b>${y}</b><br/>',
@@ -77,29 +107,150 @@ def chart_data(request):
 
 def sankey_chart(request):
     
+    #get category names, should these be helper functions?
     setCategories = expenseReport.objects.values("expenseChoices")
     newSet = list(set([i[1] for s in [d.items() for d in setCategories] for i in s]))
+    newSet.remove("INCOME")
     
-    #need to create something to dynamically mimic the sankey chart and build up the lists
+    data = []
     
+    #get current dates
+    dateNow = datetime.now()
+    currentYear = dateNow.year
+    currentMonth = dateNow.month
+    
+    #get the income total
+    incomeSum = expenseReport.objects.values("value"
+                    ).filter(
+                        date__year=currentYear, date__month=currentMonth
+                    ).filter(
+                        expenseChoices=str("INCOME")).aggregate(
+                    Sum('value'))['value__sum']
+              
+    #check for no income          
+    if incomeSum == None:
+        incomeSum = 0
+               
+    #get all expenses  
+    expensesSum = 0  
+    expensesDict = {}
+                       
+    for i in newSet:
+        categorySum = expenseReport.objects.values("value"
+                    ).filter(
+                        date__year=currentYear, date__month=currentMonth
+                    ).filter(
+                        expenseChoices=str(i)).aggregate(
+                    Sum('value'))['value__sum']
+        if categorySum == None:
+            continue
+        
+        expensesDict[i] = float(categorySum)
+        
+        expensesSum += float(categorySum)
+        
+    #calculate income and savings
+    incomeSum = float(incomeSum)
+    savingsSum = incomeSum - expensesSum
+        
+    #append for json file                
+    data.append(
+        ['Income','Savings',savingsSum]
+    )
+    data.append(
+        ['Income','Expenses',expensesSum]
+    )
+
+    for index, key in expensesDict.items():
+        data.append(
+            ['Expenses',index,key]
+        )
+
+    #add if else when chart wants to be changed
+    chartTitle = ""
+    if True:
+        chartTitle = f"Expense and Savings This Month {currentMonth}/{currentYear}"
+    # else:
+    #     chartTitle = "Total Expenses All"
+    
+    #chart json
     chart = {
-        'chart': {'type': 'sankey', 'renderTo':'container0'},
-        'title': {'text': 'Income and Savings Breakdown'},
-        'tooltip': {},
-        'keys':['from','to','weight'],
-        'data': [],
+        'chart': {'type': 'sankey','renderTo':'expenses-sankey-container'},
+        'title': {'text': chartTitle},
+        'tooltip': {
+        'headerFormat': None,
+        'pointFormat':
+      '{point.fromNode.name} \u2192 {point.toNode.name}: ${point.weight:.2f}',
+        'nodeFormat': '{point.name}: ${point.sum:.2f}'
+    },
+        'series': [{
+            'name':'Amount',
+            'keys':['from','to','weight'],
+            'nodes': [
+                {
+                    'id':'Income',
+                    'color':'#adf5a1'
+                },
+                {
+                    'id':'Savings',
+                    'color':'#adf5a1'
+                },
+                {
+                    'id':'Expenses',
+                    'color':'#ff7676'
+                },
+                {
+                    'id':'GROCERIES',
+                    'color':'#ff7676'
+                },
+                {
+                    'id':'GAS_REPAIRS',
+                    'color':'#ff7676'
+                },
+                {
+                    'id':'DINE_OUT',
+                    'color':'#ff7676'
+                },
+                {
+                    'id':'RENT_MORTGAGE',
+                    'color':'#ff7676'
+                },
+                {
+                    'id':'TAXES',
+                    'color':'#ff7676'
+                },
+                {
+                    'id':'UTILITIES',
+                    'color':'#ff7676'
+                },
+                {
+                    'id':'GIVING',
+                    'color':'#ff7676'
+                },
+                {
+                    'id':'HEALTHCARE',
+                    'color':'#ff7676'
+                },
+                {
+                    'id':'HOME_IMPROVEMENT',
+                    'color':'#ff7676'
+                },
+                {
+                    'id':'SHOPPING',
+                    'color':'#ff7676'
+                },
+                {
+                    'id':'TRAVEL',
+                    'color':'#ff7676'
+                },
+                {
+                    'id':'ENTERTAINMENT',
+                    'color':'#ff7676'
+                },
+            ],
+            'data': data,
+            'type':'sankey',
+        }]
     }
     
     return JsonResponse(chart)
-
-        # ['Total Assets', 'Current-Assets', 143.71],
-        # ['Total Assets', 'Non-Current Assets', 180.17],
-
-        # ['Current-Assets', 'Cash', 90.94],
-        # ['Current-Assets', 'Receivables', 37.44],
-        # ['Current-Assets', 'Inventory', 4.06],
-        # ['Current-Assets', 'Other CA', 11.26],
-
-        # ['Non-Current Assets', 'Net PPE ', 36.76],
-        # ['Non-Current Assets', 'Investments ', 100.88],
-        # ['Non-Current Assets', 'Other NCA', 42.52]
